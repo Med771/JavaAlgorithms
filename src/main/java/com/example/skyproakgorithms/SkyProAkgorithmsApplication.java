98c8eade55e3e662673f2fb73cc7aa17c0761203
Med771 <mm7737333@gmail.com>
2024-11-18 10:46:20 +0300

    Begin

---
+++ src/main/java/com/example/skyproakgorithms/SkyProAkgorithmsApplication.java
@@ -0,0 +1,14 @@
+package com.example.skyproakgorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+
+@SpringBootApplication
+public class SkyProAkgorithmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyProAkgorithmsApplication.class, args);
+    }
+}
